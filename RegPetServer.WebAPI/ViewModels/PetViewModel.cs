a9b8d201e7384bd4151b87f8b2fa91caad29f3f5
Tihomir Arahangelov <angelov.tisho@gmail.com>
2015-01-09 06:23:17 +0200

    Update

---
+++ RegPetServer.WebAPI/ViewModels/PetViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RegPetServer.WebAPI.ViewModels
+{
+    public class PetViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Owner { get; set; }
+
+        public string PetCategory { get; set; }
+
+        public string Breed { get; set; }
+    }
+}
